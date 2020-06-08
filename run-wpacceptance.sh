688ef94e80daa31b4d043d91f348afcddcdfb86f
Tung Du <dinhtungdu@gmail.com>
2020-06-08 20:33:04 +0700

    test only external connections

--- run-wpacceptance.sh
+++ run-wpacceptance.sh
@@ -1,6 +1,6 @@
 for i in 1 2 3; do
 	echo -e "\033[32mRunning wp-acceptance with snapshot $1\033[0m"
-	./vendor/bin/wpacceptance run --snapshot_id=$1 --enforce_clean_db
+	./vendor/bin/wpacceptance run --snapshot_id=$1
 	EXIT_CODE=$?
 
 	if [ $EXIT_CODE -gt 1 ]; then
--- tests/wpacceptance/BlocksTest.php
+++ tests/wpacceptance/BlocksTest.php
@@ -27,64 +27,6 @@ class BlocksTests extends \TestCase {
 	}
 
 	/**
-	 * Test network pulling content with blocks.
-	 */
-	public function testBlocksNetworkPulledContent() {
-		$I = $this->openBrowserPage();
-
-		$I->loginAs( 'wpsnapshots' );
-		$I->moveTo( '/wp-admin/post.php?post=40&action=edit' );
-
-		// Only test for the block editor.
-		if ( ! $this->editorHasBlocks( $I ) ) {
-			return;
-		}
-		$this->addContentToTestPost( $I );
-		$post_info = $this->pullPost( $I, 40, 'two', '' );
-		$I->moveTo( $post_info['distributed_edit_url'] );
-		$I->waitUntilElementVisible( '#wpadminbar' );
-		$this->dismissNUXTip( $I );
-
-		// Grab the post content.
-		$source = $I->getPageSource();
-
-		// Test the distributed post content.
-		$this->assertTrue(
-			(bool) preg_match( '<!-- wp:paragraph -->', $source ),
-			'Blocks was not pulled properly over a network connection'
-		);
-	}
-
-	/**
-	 * Test network pushing content with blocks.
-	 */
-	public function testBlocksNetworkPushedContent() {
-		$I = $this->openBrowserPage();
-
-		$I->loginAs( 'wpsnapshots' );
-		$I->moveTo( '/wp-admin/post.php?post=40&action=edit' );
-
-		// Only test for the block editor.
-		if ( ! $this->editorHasBlocks( $I ) ) {
-			return;
-		}
-		$this->addContentToTestPost( $I );
-		$post_info = $this->pushPost( $I, 40, 2 );
-
-		// Now let's navigate to the new post
-		$I->moveTo( $post_info['distributed_edit_url'] );
-		$I->waitUntilElementVisible( '#wpadminbar' );
-
-		// Check that the blocks are intact by looking for the paragraph comment.
-		$source = $I->getPageSource();
-
-		$this->assertTrue(
-			(bool) preg_match( '<!-- wp:paragraph -->', stripslashes( $source ) ),
-			'Blocks were not pushed properly over an external connection'
-		);
-	}
-
-	/**
 	 * Test external pushing content with blocks.
 	 */
 	public function testBlocksExternalPushedContent() {
