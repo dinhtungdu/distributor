c264ae7e495fefd66b0f9264be48846491dd6268
Tung Du <dinhtungdu@gmail.com>
2020-06-18 21:57:32 +0700

    test: remove more cases

--- tests/wpacceptance/BlocksTest.php
+++ tests/wpacceptance/BlocksTest.php
@@ -14,8 +14,6 @@ class BlocksTests extends \TestCase {
 	public function addContentToTestPost( $I ) {
 		$I->moveTo( '/wp-admin/post.php?post=40&action=edit' );
 
-		$I->refresh();
-
 		try {
 			$I->getElement( '.editor-default-block-appender__content' );
 			$I->waitUntilElementVisible( '.editor-default-block-appender__content' );
@@ -104,109 +102,4 @@ class BlocksTests extends \TestCase {
 		);
 	}
 
-	/**
-	 * Test external pushing content with blocks.
-	 */
-	public function testBlocksExternalPushedContent() {
-		$I = $this->openBrowserPage();
-
-		$I->loginAs( 'wpsnapshots' );
-		$I->moveTo( '/wp-admin/post.php?post=40&action=edit' );
-
-		// Only test for the block editor.
-		if ( ! $this->editorHasBlocks( $I ) ) {
-			return;
-		}
-		$this->addContentToTestPost( $I );
-		$I->moveTo( 'wp-admin/post-new.php?post_type=dt_ext_connection' );
-
-		$I->click( '.manual-setup-button' );
-
-		$I->typeInField( '#title', 'Test External Connection' );
-
-		$I->typeInField( '#dt_username', 'wpsnapshots' );
-
-		$I->typeInField( '#dt_external_connection_url', $this->getWPHomeUrl() . '/two/wp-json' );
-
-		$I->typeInField( '#dt_password', 'password' );
-
-		$I->waitUntilElementContainsText( 'Connection established', '.endpoint-result' );
-
-		$I->click( '#create-connection' );
-
-		$I->waitUntilElementVisible( '.notice-success' );
-		$url = $I->getCurrentUrl();
-		preg_match( '/post=(\d+)/', $url, $matches );
-
-		$post_info = $this->pushPost( $I, 40, (int) $matches[1], '', 'publish', true );
-		$I->moveTo( 'two/wp-admin/edit.php' );
-
-		// Switch to the distributed post.
-		$I->waitUntilElementVisible( '#the-list' );
-		$I->click( 'a.row-title' );
-		$I->waitUntilNavigation();
-
-		// Use moveTo to prime page object.
-		$url = $I->getCurrentUrl();
-		$I->moveTo( 'two/wp-admin/edit.php' );
-		$I->moveTo( $url );
-
-		$I->waitUntilElementVisible( 'body.post-php' );
-
-		// Check that the blocks are intact by looking for the paragraph comment.
-		$source = $I->getPageSource();
-
-		$this->assertTrue(
-			(bool) preg_match( '<!-- wp:paragraph -->', stripslashes( $source ) ),
-			'Blocks were not pushed properly over an external connection'
-		);
-	}
-
-	/**
-	 * Test external pulling content with blocks.
-	 */
-	public function testBlocksExternalPulledContent() {
-		$I = $this->openBrowserPage();
-
-		$I->loginAs( 'wpsnapshots' );
-		$I->moveTo( '/wp-admin/post.php?post=40&action=edit' );
-
-		// Only test for the block editor.
-		if ( ! $this->editorHasBlocks( $I ) ) {
-			return;
-		}
-		$this->addContentToTestPost( $I );
-		$I->moveTo( 'two/wp-admin/post-new.php?post_type=dt_ext_connection' );
-
-		$I->click( '.manual-setup-button' );
-
-		$I->typeInField( '#title', 'Test External Connection' );
-
-		$I->typeInField( '#dt_username', 'wpsnapshots' );
-
-		$I->typeInField( '#dt_external_connection_url', $this->getWPHomeUrl() . '/wp-json' );
-
-		$I->typeInField( '#dt_password', 'password' );
-
-		$I->waitUntilElementContainsText( 'Connection established', '.endpoint-result' );
-
-		$I->click( '#create-connection' );
-
-		$I->waitUntilElementVisible( '.notice-success' );
-
-		// Pull post from external connection.
-		$post_info = $this->pullPost( $I, 40, 'two', '', 'Test External Connection' );
-		$I->moveTo( $post_info['distributed_edit_url'] );
-		$I->waitUntilElementVisible( '#wpadminbar' );
-
-		// Check that the blocks are intact by looking for the paragraph comment.
-		$source = $I->getPageSource();
-		$this->assertTrue(
-			(bool) preg_match( '<!-- wp:paragraph -->', stripslashes( $source ) ),
-			'Blocks were not pushed properly over an external connection'
-		);
-	}
-
-
 }
-
